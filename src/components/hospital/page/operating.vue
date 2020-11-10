ab57a9ccfcc2a15f2c871bdea4e58f946070de69
BlockHead <1955372313@qq.com>
2020-11-10 11:53:11 +0800

    s

--- src/components/hospital/page/operating.vue
+++ src/components/hospital/page/operating.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="operating">
-        
+        <topNavBottomBorder>
+            <img draggable="false" slot="returnImg" src="../../../assets/shape@3x.png" alt=""  @click="$common.backFn" id="navback">
+			<h1 slot="title">运营手册</h1>
+            <img draggable="false" slot="rightNav" src="../../../assets/jilu@2x.png" alt="">
+        </topNavBottomBorder>
     </div>
 </template>
 <script>
--- src/components/moduleComponents/topNav_bottomBorder.vue
+++ src/components/moduleComponents/topNav_bottomBorder.vue
@@ -7,7 +7,8 @@
 			<div class="centerNav">
 				<slot name="title"></slot>
 			</div>
-			<div>
+			<div class="rightNav"> 
+				<slot name="rightNav"></slot>
 			</div>
 		</div>
 		<div style="height:30px"></div>
@@ -38,6 +39,8 @@ export default {
 	background-color: #FFFFFF;
 	border-bottom: 1px solid #efecec;
 	box-shadow: rgba(217, 211, 211, 0.5) 0px 1px 2px 0px;
+	display: flex;
+	justify-content:space-between
 }
 .zhangwei{
 	width: 100%;
@@ -46,20 +49,23 @@ export default {
 .backImg img{
 	width: 9px;
 	height: 15px;
-	position: absolute;
-	left: 16px;
-	top:16px;
+	object-fit: cover;
+	vertical-align: middle;
+	/* position: absolute; */
+	/* left: 16px; */
+	/* top:16px; */
 }
 .backImg{
 	width: 10%;
 	height: 100%;
 	/* display: inline; */
-	float:left;
+	/* float:left; */
+	line-height: 47px;
+	/* vertical-align: middle */
 }
 .centerNav{
 	width: 80%;
 	height: 100%;
-	float:left;
 }
 .centerNav h1{
 	font-size: 18px;
@@ -68,5 +74,15 @@ export default {
     display: inline-block;
     margin-block-start: 0rem !important;
 	margin-block-end: 0rem !important;
+}
+.rightNav{
+	width: 10%;
+	height: 100%;
+}
+.rightNav>img{
+	width: 20px;
+	height: 19px;
+	object-fit: cover;
+	vertical-align: middle;
 }
 </style>
